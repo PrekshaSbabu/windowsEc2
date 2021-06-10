281ddf5c0353cec1486c1f54132d23676cc318ec
PrekshaSbabu <malupreksha98@gmail.com>
2021-06-10 16:14:20 +0700

    windows vm creation file

---
+++ ec2.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-2"
+}
+resource "aws_instance" "example" {
+  ami               = "ami-086850e3dda52e84a"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-2a"
+  security_groups   = ["${aws_security_group.allow_rdp.name}"]
+  key_name = "keyec2"
+}
+resource "aws_security_group" "allow_rdp" {
+  name        = "allow_rdp"
+  description = "allow ssh traffic"
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
