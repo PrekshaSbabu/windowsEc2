df277b122dcb19b43ff88d925b2c0c368c81dbee
PrekshaSbabu <84118268+PrekshaSbabu@users.noreply.github.com>
2021-06-13 11:19:21 +0530

    updated

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "example" {
   ami               = "ami-086850e3dda52e84a"
   instance_type     = "t2.micro"
   availability_zone = "us-east-2a"
-  security_groups   = ["${aws_security_group.allow_rdp.name}"]
+  security_groups   = ["${aws_security_group.allow_rdp1.name}"]
   key_name = "keyec2"
 }
 resource "aws_security_group" "allow_rdp1" {
