123868b078dbbf101a5b2051bf4eda731ecc40d5
PrekshaSbabu <84118268+PrekshaSbabu@users.noreply.github.com>
2021-06-13 11:34:55 +0530

    updated

--- ec2.tf
+++ ec2.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "example" {
-  ami               = "ami-086850e3dda52e84a"
+  ami               = "ami-0fa60543f60171fe3"
   instance_type     = "t2.micro"
   availability_zone = "us-east-1a"
-  security_groups   = ["${aws_security_group.allow_rdp1.name}"]
+  security_groups   = ["${aws_security_group.allow_rdp.name}"]
   key_name = "keyec2"
 }
-resource "aws_security_group" "allow_rdp1" {
-  name        = "allow_rdp1"
+resource "aws_security_group" "allow_rdp" {
+  name        = "allow_rdp"
   description = "allow ssh traffic"
 
   ingress {
